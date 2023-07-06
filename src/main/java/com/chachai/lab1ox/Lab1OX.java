19a03d5db46149856f47664d76379bb194a75310
Praewwarin Chachai <64160262@go.buu.ac.th>
2023-07-06 14:24:41 +0700

    start project

---
+++ src/main/java/com/chachai/lab1ox/Lab1OX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.chachai.lab1ox;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Lab1OX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
