ea17e5753e35cee046c0ad138724a72062872ad9
Thanawuth Rodcharoen <64160049@go.buu.ac.th>
2023-07-06 20:27:21 +0700

    checkwins

--- src/main/java/com/chachai/lab1ox/Lab1OX.java
+++ src/main/java/com/chachai/lab1ox/Lab1OX.java
@@ -13,19 +13,16 @@ public class Lab1OX {
     public int row;
     public int col;
     Scanner kb = new Scanner(System.in);
-    public boolean play;
+    public boolean play = false;
     public String start;
-    public String reset;
-    public String end;
+    public boolean end;
     public String turn;
-    public String[][] board = {{" _ |", "_ |", "_"}, {" _ |", "_ |", "_"}, {" _ |", "_ |", "_"}, {"   |", "  |", " "}};
-    
-    public void startGame() {
+    public String[][] board = {{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}};
 
+    public void startGame() {
         System.out.println("Welcome to  OX game!!!");
         System.out.print("Start Game??? (y/n): ");
         start = kb.nextLine().toLowerCase();
-
         while (!start.equals("n") && !start.equals("y")) {
             System.out.print("Start Game (y/n): ");
             start = kb.nextLine().toLowerCase();
@@ -37,19 +34,23 @@ public class Lab1OX {
         }
 
     }
-    
-    public void inputRowAndCol(){
+
+    public void inputRowAndCol() {
         System.out.print("Input row : ");
-         row = kb.nextInt();
-         System.out.print("Input column : ");
-         col = kb.nextInt();
-         while(((row > 0 && row < 4) && (col > 0 && col < 4))){
-             
-            if (board[row - 1][col - 1].equals("-")) {
-                 board[row - 1][col - 1] = turn.toUpperCase();
+        row = kb.nextInt();
+        System.out.print("Input column : ");
+        col = kb.nextInt();
+        if (((row > 0 && row < 4) && (col > 0 && col < 4))) {
+
+            if (board[row - 1][col - 1].equals("_")) {
+
+                board[row - 1][col - 1] = turn.toUpperCase();
+                checkWin();
+                nextTurn();
+
             } else {
 
-                while (((row > 0 && row < 4) && (col > 0 && col < 4)) && (!board[row - 1][col - 1].equals("-"))) {
+                while ((!(row > 0 && row < 4) && !(col > 0 && col < 4)) && !(board[row - 1][col - 1].equals("_"))) {
 
                     System.out.println("Please Input Again.");
                     System.out.print("Input row :");
@@ -61,68 +62,98 @@ public class Lab1OX {
 
                 return;
             }
-         }
-    }
-    
-    
-    
-    public void endGame() {
-        System.out.print("Do you want to Exit ??? (y/n): ");
-        String end = kb.nextLine().toLowerCase();
 
-        while (!end.equals("n") && !end.equals("y")) {
-            System.out.print("Do you want to Exit ??? (y/n): ");
-            end = kb.nextLine().toLowerCase();
-        }
-        if (end.equals("n")) {
-            play = false;
         } else {
-            play = true;
+            return;
+        }
+    }
+
+    public void nextTurn() {
+        if (turn.equals("x")) {
+            turn = "o";
+        } else {
+            turn = "x";
         }
 
     }
-     public void reset() {
+
+    public boolean checkDraw() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                board[i][j] = "-";
+                if (board[i][j].equals("_")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void checkWin() {
+        if (checkRows()) {
+            play = false;
+            System.out.println("+--------------------+");
+            System.out.println("|    !!! " + turn + " Wins !!!   |");
+            System.out.println("+--------------------+");
+        } if(play == true){
+            if(checkDraw()){
+            System.out.println("+--------------------+");
+            System.out.println("|    !!! Draw !!!    |");
+            System.out.println("+--------------------+");
+            play = false;
             }
         }
     }
-    
-    public void nextTurn() {
-        if (turn.equals("X")) {
-            turn = "O";
-        } else {
-            turn = "X";
+
+    public boolean checkRows() {
+        for (int j = 0; j < board[row - 1].length; j++) {
+            if (!board[row - 1][j].toLowerCase().equals(turn)) {
+                return false;
+            }
         }
-
-    }
-
-     public void showTurn() {
-         System.out.println("_________________________");
-         System.out.println("Turn >>> " + turn.toUpperCase());
-        
-
+        return true;
     }
 
 
-    public void showBoard(){
-        for(int i=1; i<board.length; i++) {
-            for(int j=0; j<board[i].length; j++){
-                System.out.print(board[i][j]+ " ");
-                
+    public void showBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+
             }
             System.out.println();
-            
+
         }
-    
+
     }
 
-    
+    public void showTurn() {
+        System.out.println("_________________________");
+        System.out.println("Turn >>> " + turn.toUpperCase());
+
+    }
+
+    public void lean() {
+        showBoard();
+        showTurn();
+        inputRowAndCol();
+
+    }
+
     public static void main(String[] args) {
+
         Lab1OX project = new Lab1OX();
-        project.showBoard();
-        
+        Scanner kb = new Scanner(System.in);
+        project.startGame();
+        if (project.play == false) {
+            System.out.println("ByeBye!!");
+            return;
+        }
+
+        project.turn = "x";
+        while (project.play) {
+            project.lean();
+        }
+
     }
         
     
