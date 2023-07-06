2189b09275be6b35aab62df421da1a0622c99a1d
Pornchitar Sarsai <kream21574@gmail.com>
2023-07-06 16:04:01 +0700

    startGame

--- src/main/java/com/chachai/lab1ox/Lab1OX.java
+++ src/main/java/com/chachai/lab1ox/Lab1OX.java
@@ -4,14 +4,27 @@
 
 package com.chachai.lab1ox;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lenovo
  */
 public class Lab1OX {
-
+    public int row;
+    public int col;
+    Scanner kb = new Scanner(System.in);
+    public boolean play;
+    
+    public void inputRowAndCol(){
+        System.out.print("Input row : ");
+         row = kb.nextInt();
+         System.out.print("Input column : ");
+         col = kb.nextInt();
+    }
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello");
+        
     }
 }
